b5bf99d7e725cf4ee8a66bd92a688c99cdde3833
UBISOFT-1 <54579186+UBISOFT-1@users.noreply.github.com>
2020-10-19 13:21:34 +0500

    Project works, detects faces and saves unknown image in Unknown_Access Folder.

---
+++ Sub_Modules_Detection.py
@@ -0,0 +1,26 @@
+import os
+import face_recognition
+def Get_Files(path_to_folder):
+    extensions = ['.jpg', '.JPG', '.jpeg', '.JPEG','.png', '.PNG']
+    good_images = []
+    files = os.listdir(path_to_folder)
+    for element in files:
+        for extension in extensions:
+            if extension in element:
+                good_images.append(element)
+    names = []
+    for element in good_images:
+        for stuff in extensions:
+            if stuff not in element:
+                continue
+            else:
+                meth = element.replace(f'{stuff}', '')
+                names.append(meth)
+    return names, good_images
+
+def Encoding(image_path):
+    image = face_recognition.load_image_file("C:/Users/ahmad/Pictures/Camera Roll/Muneeb Ahmad.jpg")
+    encoding = face_recognition.face_encodings(image)[0]
+    return encoding
+
+
